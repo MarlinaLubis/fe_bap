5a30877eb5fa08265c2f6fe6ba7dc9946cdc327e
Marlina Lubis <lubismarlina5@gmail.com>
2023-04-07 20:04:51 +0700

    Fe-Bap

--- js/table.js
+++ js/table.js
@@ -9,10 +9,10 @@ export let isiTabel =
             <div class="w-auto p-2">
                 <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
             </div>
-            // <div class="w-auto p-2">
-            //     <p class="text-xs font-semibold text-coolGray-800">#JUDUL#</p>
-            //     <p class="text-xs font-medium text-coolGray-500">#NAMAMAHASISWA#</p>
-            // </div>
+            <div class="w-auto p-2">
+                <p class="text-xs font-semibold text-coolGray-800">#JUDUL#</p>
+                <p class="text-xs font-medium text-coolGray-500">#NAMAMAHASISWA#</p>
+            </div>
         </div>
     </th>
     <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#JUDUL#</th>
