31e3422ac33546824c8fcdf6d7ae8687c533c3af
Marlina Lubis <lubismarlina5@gmail.com>
2023-04-07 17:08:35 +0700

    Fe-Marlina

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,20 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://marlinapp.herokuapp.com/bap";
-get(urlAPI,isiTableBap);
-function isiTableBap(results){
-    console.log(results);
+get(urlAPI, isiTableBap);
+function isiTableBap(results) {
+    results.forEach(isiRow);
+}
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#JUDUL#", value.biodata.judul)
+            .replace("#NAMAMAHASISWA#", value.biodata.nama_mahasiswa)
+            .replace("#PRODI#", value.biodata.prosi)
+            .replace("#HASILREVISI#", value.hasil_revisi)
+            .replace("#DOSEN#", value.biodata.dosen)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
 }
