974173a4ebcde465355792b23746d123f0691658
Marlina Lubis <lubismarlina5@gmail.com>
2023-04-07 17:14:59 +0700

    Fe-Marlina

--- js/table.js
+++ js/table.js
@@ -10,20 +10,18 @@ export let isiTabel =
                 <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
             </div>
             <div class="w-auto p-2">
-                <p class="text-xs font-semibold text-coolGray-800">#NAMA#</p>
-                <p class="text-xs font-medium text-coolGray-500">#NOHP#</p>
+                <p class="text-xs font-semibold text-coolGray-800">#JDUL#</p>
+                <p class="text-xs font-medium text-coolGray-500">#NAMAMAHASISWA#</p>
             </div>
         </div>
     </th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#JABATAN#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#STATUS#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#LOKASI#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#HARIKERJA#</th>
-    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-#col#-500 text-left">#JAMKERJA#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#PRODI#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#HASILREVISI#</th>
+    <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-500 text-left">#DOSEN#</th>
     <th class="whitespace-nowrap px-4 bg-white text-left">
         <div class="w-auto p-2">
-        <p class="text-xs font-semibold text-coolGray-800">#JAMMASUK#</p>
-        <p class="text-xs font-medium text-coolGray-500">#JAMKELUAR#</p>
+        <p class="text-xs font-semibold text-coolGray-800">#HASILREVISI#</p>
+        <p class="text-xs font-medium text-coolGray-500">#DOSEN#</p>
         </div>
     </th>
     <th class="whitespace-nowrap pr-4 bg-white text-sm font-medium text-coolGray-800">
