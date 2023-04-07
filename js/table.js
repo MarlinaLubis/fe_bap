d9939788409f0fcdc22759cc006fa46ac9346779
Marlina Lubis <lubismarlina5@gmail.com>
2023-04-07 21:48:55 +0700

    FE_BAP

--- js/table.js
+++ js/table.js
@@ -10,7 +10,8 @@ export let isiTabel =
                 <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
             </div>
             <div class="w-auto p-2">
-            <p class="text-xs font-semibold text-coolGray-800">#JUDUL#</p>
+                <p class="text-xs font-semibold text-coolGray-800">#LOGO#</p>
+                <p class="text-xs font-medium text-coolGray-500">#JUDUL#</p>
             </div>
         </div>
     </th>
