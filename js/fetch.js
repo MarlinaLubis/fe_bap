1bfef6a9ca32a28fc54f96dfd6038eaab5fc1855
Marlina Lubis <lubismarlina5@gmail.com>
2023-04-10 14:28:34 +0700

    Fe-Marlina

--- js/fetch.js
+++ js/fetch.js
@@ -6,17 +6,15 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://marlinapp.herokuapp.com/bap";
 get(urlAPI, isiTableBap);
 function isiTableBap(results) {
   results.forEach(isiRow);
 }
-
 function isiRow(value) {
     let content = 
-    isiTabel
-            .replace("#JUDUL#", value.judul)
+    isiTabel.replace("#JUDUL#", value.judul)
             .replace("#NAMASISWA#", value.nama_mahasiswa)
             .replace("#PRODI#", value.prodi)
             .replace("#HASILREVISI#", value.hasil_revisi)
