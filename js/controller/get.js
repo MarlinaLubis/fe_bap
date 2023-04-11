324f5d10ddd46a126d76a1553e172cb4e33da40a
Marlina Lubis <lubismarlina5@gmail.com>
2023-04-11 16:33:10 +0700

    Fe-Bap

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,22 +1,20 @@
-// import { get } from "https://bukulapak.github.io/api/process.js";
-// import { addInner } from "https://bukulapak.github.io/element/process.js";
-// import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-// import { isiTabel } from "../temp/table.js";
-// let urlAPI = "https://marlinapp.herokuapp.com/bap";
-// get(urlAPI, isiTableBap);
-// function isiTableBap(results) {
-//   results.forEach(isiRow);
-// }
-
-// function isiRow(value) {
-//     let content = 
-//     isiTabel
-//             .replace("#JUDUL#", value.judul)
-//             .replace("#NAMASISWA#", value.nama_mahasiswa)
-//             .replace("#PRODI#", value.prodi)
-//             .replace("#HASILREVISI#", value.hasil_revisi)
-//             .replace("#DOSEN#", value.dosen)
-//             .replace("#WARNA#", getRandomColor())
-//             .replace(/#WARNALOGO#/g, getRandomColorName());
-//         addInner("iniTabel", content);
-// }
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
+let urlAPI = "https://marlinapp.herokuapp.com/bap";
+get(urlAPI, isiTableBap);
+function isiTableBap(results) {
+    results.forEach(isiRow);
+}
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#JUDUL#", value.judul)
+            .replace("#NAMAMAHASISWA#", value.nama_mahasiswa)
+            .replace("#PRODI#", value.prodi)
+            .replace("#HASILREVISI#", value.hasil_revisi)
+            .replace("#DOSEN#", value.dosen)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
+}
--- js/fetch.js
+++ js/fetch.js
@@ -1,25 +1,5 @@
-// import { get } from "https://bukulapak.github.io/api/process.js";
-// import { isiTableBap } from "./controller/get.js";
-// import { urlAPI } from "./config/url.js";
-// get(urlAPI, isiTableBap);
-
 import { get } from "https://bukulapak.github.io/api/process.js";
-import { addInner } from "https://bukulapak.github.io/element/process.js";
-import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "./table.js";
-let urlAPI = "https://marlinapp.herokuapp.com/bap";
+import { isiTableBap } from "./controller/get.js";
+import { urlAPI } from "./config/url.js";
 get(urlAPI, isiTableBap);
-function isiTableBap(results) {
-    results.forEach(isiRow);
-}
-function isiRow(value) {
-    let content = 
-    isiTabel.replace("#JUDUL#", value.judul)
-            .replace("#NAMAMAHASISWA#", value.nama_mahasiswa)
-            .replace("#PRODI#", value.prodi)
-            .replace("#HASILREVISI#", value.hasil_revisi)
-            .replace("#DOSEN#", value.dosen)
-            .replace("#WARNA#", getRandomColor())
-            .replace(/#WARNALOGO#/g, getRandomColorName());
-        addInner("iniTabel", content);
-}
+
